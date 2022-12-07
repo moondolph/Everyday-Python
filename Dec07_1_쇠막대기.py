9a5440d7deed9e5cd0c83cfd40696565b408868e
DESKTOP-8B0P6NC\LG <jysim1000@gmail.com>
2022-12-07 22:51:40 +0900

    2022-12-07 commit

--- Dec07_1_쇠막대기.py
+++ Dec07_1_쇠막대기.py
@@ -1,5 +1,5 @@
-'''
-a=list(input().split())
+'''얘는 조건 비교후 값을 저장해야하는데 얘는 순서의 일관성이 없다 
+a=list(input())
 stack=0
 sum=0
 for i in range(len(a)):
@@ -7,14 +7,14 @@ for i in range(len(a)):
         if a[i+1]=="(":
             stack+=1
         else:
-            stack-=1
             sum+=stack
-    if a[i]==")":
-        if a[i+1]=="(":
+    else:
+        if a[i+1]==")":
             stack-=1
             sum+=1
-        else:
+print(sum)
 '''
+
 
 s=input()
 stack=[]
@@ -26,8 +26,28 @@ for i in range(len(s)):
         #stack.pop()<-- 밑에 중복되니까 여기다 써도 됨
         if s[i-1]=='(':         
             stack.pop()
-            cnt+=len(stack)
+            cnt+=len(stack) #stack에 담겨있는 '(' 의 개수만큼 레이저 맞았을때 더해줌
         else:
             stack.pop()
             cnt+=1
 print(cnt)
+
+
+''' 처음부터 stack은 '('를 담는 개수 
+a=list(input())
+stack=0
+sum=0
+for i in range(len(a)):
+    if a[i]=="(":
+        stack+=1
+        if a[i+1]==")":
+            stack-=1
+
+    else:
+        if a[i-1]=="(":
+            sum+=stack
+        else:
+            stack-=1
+            sum+=1
+print(sum)
+'''
